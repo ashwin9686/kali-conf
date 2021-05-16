938c1d831a7796beae89db1a4e4bd0c020647fe0
ashwin9686 <ashwin9686@gmail.com>
2021-05-16 07:18:26 -0400

    Update setup-script.sh
    Renaming pip installation script

--- setup-script.sh
+++ setup-script.sh
@@ -42,6 +42,9 @@ echo "Done installing html2text"
 sudo apt install -y crowbar
 echo "Done installing crowbar"
 
+sudo apt install -y burpsuite
+echo "Done installing burpsuite" 
+
 sudo apt install -y pure-ftpd
 echo "Done installing pureFTP"
 
@@ -71,10 +74,10 @@ cd /home/kali/scriptsForOSCP
 wget https://raw.githubusercontent.com/diego-treitos/linux-smart-enumeration/master/lse.sh -O /home/kali/scriptsForOSCP/lse.sh
 echo "Done downloading lse.sh" 
 
-wget https://bootstrap.pypa.io/2.7/get-pip.py -O /home/kali/scriptsForOSCP/python2-pip-installation-script.py
+wget https://bootstrap.pypa.io/2.7/get-pip.py -O /home/kali/scriptsForOSCP/get-pip.py
 echo "Done downloading python2-pip installation script"
 
-python /home/kali/scriptsForOSCP/python2-pip-installation-script.py
+python /home/kali/scriptsForOSCP/get-pip.py
 echo "Done installng python2-pip"
 
 pip install --upgrade setuptools
