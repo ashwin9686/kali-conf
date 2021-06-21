b64ce0d68f660fdf49c5e3ec932d62fbc3af479c
ashwin9686 <ashwin9686@gmail.com>
2021-06-21 21:26:52 +0530

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -3,59 +3,17 @@
 sudo apt update
 echo "Done updating the apt cache"
 
-sudo apt install -y seclists
-echo "Done installing seclists"
+sudo apt -y upgrade 
+echo "Done upgrade all packages"
 
-sudo apt install -y tmux
-echo "Done installing tmux"
-
-sudo apt install -y tilix
-echo "Done installing tilix"
-
-sudo apt install -y davtest
-echo "Done installing davtest"
+sudo apt install -y seclists tmux tilix davtest exiftool masscan gcc-multilib wpscan telnet html2text crowbar burpsuite pure-ftpd remmina cadaver gobuster
+echo "Done installing extra packages"
 
 cd /home/kali/
 echo "Changing directory to home"
 
 wget https://raw.githubusercontent.com/ashwin9686/kali-conf/main/.tmux.conf -O /home/kali/.tmux.conf 
 echo "Done setting up tmux configuration"
- 
-sudo apt install -y exiftool
-echo "Done installing exiftool"
-
-sudo apt install -y masscan
-echo "Done installing masscan" 
-
-sudo apt install -y gcc-multilib
-echo "Done installing gcc-multilib"
-
-sudo apt install -y wpscan
-echo "Done installing wpscan"
-
-sudo apt install -y telnet
-echo "Done installing telnet"
-
-sudo apt install -y html2text
-echo "Done installing html2text"
-
-sudo apt install -y crowbar
-echo "Done installing crowbar"
-
-sudo apt install -y burpsuite
-echo "Done installing burpsuite" 
-
-sudo apt install -y pure-ftpd
-echo "Done installing pureFTP"
-
-sudo apt install -y remmina
-echo "Done installing remmina"
-
-sudo apt install -y cadaver
-echo "Done installing cadaver for WebDAV"
-
-sudo apt install -y gobuster
-echo "Done installing Gobuster" 
 
 sudo apt install -y atftp
 sudo mkdir /tftp
